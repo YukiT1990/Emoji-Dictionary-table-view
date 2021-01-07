3bc9aaf2ff2ec2fd66de4b11c5b7b2c7f36d2f0b
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-08 06:15:58 +0900

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Yuki Tsukada on 2021/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
